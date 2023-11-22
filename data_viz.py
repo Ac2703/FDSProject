7cd96f77c2e8d0539d708e978620dd487416be64
Avvai Chandrasekaran <115323976+Ac2703@users.noreply.github.com>
2023-11-22 18:05:02 -0500

    Add files via upload

---
+++ data_viz.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import data_processing as data_proc
+import scpy as 
+
+
+file = pd.read_csv("CLEANED_2022_Property_Sales.csv")
+
+mainDF = data_proc.mainDF
+
+# ---- MONTH VS SALES PRICE---- #
+ypoints = scipy.stats.binned_statistic_mainDF['Sale_price']
+plt.scatter(mainDF['Sale_Month'].unique(), mainDF['Sale_price'])
+
+plt.show()
